45375a76456ecd5386fa7ee16936f1e69d6fee44
dimitars2003 <72808611+dimitars2003@users.noreply.github.com>
2024-10-17 19:25:50 +0300

    Update Susi.java
    plan 1

--- Susi.java
+++ Susi.java
@@ -3,31 +3,73 @@ import java.util.List;
 
 import java.util.Scanner;
 
+//trqbva console manager koito polzva susi
+
+//Student i teacher get za user
+
+//sazdavane na kurs - teacher ili admin
+//dobavqne na asistenti - teacher ili admin
+
 public class Susi {
-	private List<Course> courses;
-	private Student currStudent;
+	private List<Course> courses;// ima li ocenka - samo teacher moje da pishe - deff stoinost 2/0 - double ili int?
+	//ima li passed - samo teacher moje da pasva (funkciq pri vikane gleda ocenki smqta sredno i ako e >2.99 minava)
+	//? kursovete darjat zapisani studenti
+	// cap na zapisani
+	//
+	private Student currStudent;//list
+	//cap za kolko kursa zapisani
 	
+	// list teachers
+	//admin class - ima person ima dostap do vsichko ?dobavqne na uchiteli?
+	//proverki za curr user
+	//dobavqne na currUser
+	//inteface za neshta na student i neshta na teacher (admin ima i dvete?)
+
+	//list admin
 	public Susi(List<Course> courses, Student curr) {
 		this.courses = courses;
 		currStudent = curr;
 		
 	}
+
+	//private finkcii za proverki?
 	
+	//what am i funkciq
+
 	
+	//create student - pri log in i log out? koi sazdava studenta - admin ili user koito se logva
+	//proverka za veche takiva useri?
 	
-	public void enrollMe() {
+	//log in 
+	//2 variant
+	// - izbor kakvo si i togava logvane - tova ???
+	// direktno logvane i tarsi prez vsichko
+	
+	// log out
+	//trqbva li da sa otdelen klas
+	
+	public void enrollMe() { //ako si admin/teacher mogat asistenti da go pravqt ili samo headteacher
 		
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		
+		//pokazva spisak s kursove
+		//proverka dali e uchitel/asistent?? v kursa
+		//izbira kurs
+		//pokazva spisak s studenti
+		//potrebitel izbira koi da zapishe
 		currStudent.enrolCourse(courses.get(n));
 		
 		in.close();
 	}
 	
-	public void removeMe() {
+	public void removeMe() { // ako si admin/teacher mogat asistenti da go pravqt ili samo headteacher
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
+		//pokazva spisak s kursove
+		//izbira kurs
+		//proverka dali e uchitel/asistent?? v kursa
+		//pokazva spisak s studenti
+		//potrebitel izbira koi da zapishe
 		currStudent.leaveCourse(courses.get(n));
 		in.close();
 	}
@@ -55,6 +97,8 @@ public class Susi {
 	public void setCurrStudent(Student currStudent) {
 		this.currStudent = currStudent;
 	}
+	//add course
 	
+	//
 	
 }

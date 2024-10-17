38eed1043b4bdd1e620304e177c19df0495f4ebc
dimitars2003 <72808611+dimitars2003@users.noreply.github.com>
2024-10-17 19:30:27 +0300

    Update Person.java
    added username and pass

--- Person.java
+++ Person.java
@@ -2,16 +2,22 @@ package test;
 
 public class Person {
 	private String name;
+	private String username;
+	private String pass;
 	private int age;
 	
 	public Person() {
 		name = "kircho";
+		username = "kircho";
+		pass = "1234"
 		age = 40;
 	}
 	
 	
-	public Person (String name,int age) {
+	public Person (String name,String username,String pass, int age) {
 		this.name = name;
+		this.username = username;
+		this.pass = pass;
 		this.age = age;
 		
 	}
@@ -28,6 +34,23 @@ public class Person {
 		this.name = name;
 	}
 
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUserName(String username) {
+		this.username = username;
+	}//trqbva li
+
+	public String getPass() {
+		return pass;
+	}
+
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}//trqbva li
 
 	public int getAge() {
 		return age;

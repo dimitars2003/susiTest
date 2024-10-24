7530c93c698453665cfb5ff1ace86c4922fbceff
dimitars2003 <72808611+dimitars2003@users.noreply.github.com>
2024-10-24 17:10:41 +0300

    Update StudentCourseLinker.java

--- StudentCourseLinker.java
+++ StudentCourseLinker.java
@@ -9,21 +9,21 @@ public class StudentCourseLinker{
   private List<Boolean> passed;
   
 
-  public StudentCourseLinker(){
+  public void StudentCourseLinker(){
       idStudents = new ArrayList<Integer>();
       idCourse = new ArrayList<Integer>();
       grades = new ArrayList<List<Integer>>();
       passed = new ArrayList<Boolean>();    
   }
 
-  public addStudentToCourse(int idSt, int idC){
+  public void addStudentToCourse(int idSt, int idC){
       idStudents.push(idSt);
       idCourse.push(idC);
       grades.push(new ArrayList<Integer>());
       passed.push(false);
   }
 
-  public addGrade(int idSt, int idC, int grade){
+  public void addGrade(int idSt, int idC, int grade){
       for(int i = 0; i < idStudents.size(), i++){
         if(idStudents.getAt(i) == idSt && idCourse.getAt(i) == idC){
           grades.getAt(i).push(grade);
@@ -31,7 +31,7 @@ public class StudentCourseLinker{
       }
   }
 
-  public passStudent(int idSt, int idC){
+  public void passStudent(int idSt, int idC){
     for(int i = 0; i < idStudents.size(), i++){
       if(idStudents.getAt(i) == idSt && idCourse.getAt(i) == idC){
         passed.set(i,true);

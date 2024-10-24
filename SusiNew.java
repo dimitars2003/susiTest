70512f02b2a94b3e5cbcdc37170b29b02d80e1d1
dimitars2003 <72808611+dimitars2003@users.noreply.github.com>
2024-10-24 16:38:41 +0300

    Update SusiNew.java

--- SusiNew.java
+++ SusiNew.java
@@ -1,1 +1,13 @@
+package test;
+import java.util.List;
 
+import java.util.Scanner;
+
+public class SusiNew{
+  private List<Person> admins;
+  private List<Students> students;
+  private List<Teacher> teachers;
+
+  private List<Course> courses;
+  
+};
